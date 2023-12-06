ec38ba7ae4b918558d15f69ea8388149947eca92
ga1212312 <satthumaulanh2001@gmail.com>
2023-12-06 20:49:32 +0700

    AddOn: Complete activate Logic

--- frontend/src/pages/home/activate.js
+++ frontend/src/pages/home/activate.js
@@ -14,10 +14,16 @@ export default function Activate() {
   const { user } = useSelector((user) => ({ ...user }));
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const[error, setError] = useState()
+  const[success, setSuccess] = useState()
+  const [activateInfo, setActivateInfo] = useState({ message: '' });
+
   const {token} = useParams()
+
   useEffect(()=>{
     activateAccount()
   },[])
+
   const activateAccount = async ()=>{
     try {
         const {data} = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/activate/`, {token},{
@@ -28,16 +34,28 @@ export default function Activate() {
     
         Cookies.set('user',JSON.stringify({...user, verified: true}))
         dispatch({type:'VERIFY',payload :true})
+        setSuccess(true)
+        setActivateInfo({message: data.message})
+
         setTimeout(()=>{
             navigate('/')
         },3000)
     } catch (error) {
         console.log(error)
+        setError(true)
+        setActivateInfo({message: error.response.data.message})
+        console.log('gag123a',activateInfo)
     }
   }
+  console.log('gagagaga',activateInfo.message) 
   return (
     <div className="home">
-      <ActivateForm header='Activate account successfully' message='Welcome to Facebook' type='success' />
+      {
+        error && <ActivateForm header={activateInfo.message} message={activateInfo.message} type='error' />
+      }
+      {
+        success && <ActivateForm header={activateInfo.message} message='Welcome To Facebook' type='success' />
+      }
       <Header />
       <LeftHome user={user} />
       <div className="home_middle">
