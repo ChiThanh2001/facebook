34f533c42d1c7dcc47c7963dd2522c9bb91c7bd8
thanhlc-kimei <satthumaulanh2001@gmail.com>
2023-12-26 15:21:58 +0700

    feature: intergrate socket.io(beta)

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const app = express();
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
 const fileUpload = require("express-fileupload");
+const path = require("path");
+const http = require("http");
+const socketIO = require("socket.io");
+
 dotenv.config();
 
 app.use(cors());
@@ -12,8 +16,31 @@ app.use(express.json());
 app.use(fileUpload({
   useTempFiles: true
 }))
-//routes
-readdirSync("./routes").map((r) => app.use("/", require(`./routes/${r}`)));
+
+// Initialize HTTP server and Socket.IO
+const server = http.createServer(app);
+const io = socketIO(server);
+
+//Load routes dynamically
+const routesPath = path.join(__dirname, "routes");
+readdirSync(routesPath).map((file) => {
+  const route = require(path.join(routesPath, file));
+  app.use("/", route);
+});
+
+
+// Socket.IO connection event
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle custom events or further logic here
+
+  // Disconnect event
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
 //database
 mongoose
   .connect(process.env.DATABASE_URL)
@@ -26,6 +53,6 @@ mongoose
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
