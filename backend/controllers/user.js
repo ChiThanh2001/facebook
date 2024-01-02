fa98590571cca6e552fe13150e747dc248972e0b
ga1212312 <satthumaulanh2001@gmail.com>
2024-01-02 22:44:49 +0700

    feature: add unfriend, delete request,and modify getprofile controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -228,11 +228,36 @@ exports.changePassword = async (req,res)=>{
 exports.getProfile = async (req,res)=>{
   try{
     const { username } = req.params
+    const user = await User.findById(req.user.id);
     const profile = await User.findOne({ username }).select("-password")
+    const friendship = {
+      friends: false,
+      following: false,
+      requestSent: false,
+      requestReceived: false,
+    };
     if(!profile){
       return res.status(200).json({ ok: false })
     }
-    return res.status(200).json(profile)
+
+    if (
+      user.friends.includes(profile._id) &&
+      profile.friends.includes(user._id)
+    ) {
+      friendship.friends = true;
+    }
+    if (user.following.includes(profile._id)) {
+      friendship.following = true;
+    }
+    if (user.requests.includes(profile._id)) {
+      friendship.requestReceived = true;
+    }
+    if (profile.requests.includes(user._id)) {
+      friendship.requestSent = true;
+    }
+    console.log(profile)
+    console.log({...profile.toObject()})
+    return res.status(200).json({...profile.toObject(), friendship})
   }catch(error){
     res.status(400).json({
       message:error.message
@@ -396,6 +421,72 @@ exports.acceptRequest = async (req, res) => {
       return res
         .status(400)
         .json({ message: "You can't accept a request from  yourself" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.unfriend = async (req, res) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const sender = await User.findById(req.user.id);
+      const receiver = await User.findById(req.params.id);
+      if (
+        receiver.friends.includes(sender._id) &&
+        sender.friends.includes(receiver._id)
+      ) {
+        await receiver.update({
+          $pull: {
+            friends: sender._id,
+            following: sender._id,
+            followers: sender._id,
+          },
+        });
+        await sender.update({
+          $pull: {
+            friends: receiver._id,
+            following: receiver._id,
+            followers: receiver._id,
+          },
+        });
+
+        res.json({ message: "unfriend request accepted" });
+      } else {
+        return res.status(400).json({ message: "Already not friends" });
+      }
+    } else {
+      return res.status(400).json({ message: "You can't unfriend yourself" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteRequest = async (req, res) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const receiver = await User.findById(req.user.id);
+      const sender = await User.findById(req.params.id);
+      if (receiver.requests.includes(sender._id)) {
+        await receiver.update({
+          $pull: {
+            requests: sender._id,
+            followers: sender._id,
+          },
+        });
+        await sender.update({
+          $pull: {
+            following: receiver._id,
+          },
+        });
+
+        res.json({ message: "delete request accepted" });
+      } else {
+        return res.status(400).json({ message: "Already deleted" });
+      }
+    } else {
+      return res.status(400).json({ message: "You can't delete yourself" });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
