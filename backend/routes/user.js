232804eeda16615561e730e387f7b389b3c6e376
thanhlc-kimei <satthumaulanh2001@gmail.com>
2024-01-02 10:03:07 +0700

    fearture: logic for add fr, cancel request, follow, unfollow, accept request

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register, activateAccount, login, auth,findUser, sendResetPasswordCode, validateResetCode, changePassword, getProfile, updateProfilePicture } = require("../controllers/user");
+const { register, activateAccount, login, auth,findUser, sendResetPasswordCode, validateResetCode, changePassword, getProfile, updateProfilePicture, addFriend, cancelRequest, follow, unfollow, acceptRequest } = require("../controllers/user");
 const { authUser } = require("../middleware/auth");
 
 router.post("/register", register);
@@ -12,5 +12,10 @@ router.post("/validateResetCode", validateResetCode);
 router.post("/changePassword", changePassword);
 router.get("/getProfile/:username", authUser, getProfile);
 router.put("/updateProfilePicture", authUser, updateProfilePicture);
+router.put("/addFriend/:id", authUser, addFriend);
+router.put("/cancelRequest/:id", authUser, cancelRequest);
+router.put("/follow/:id", authUser, follow);
+router.put("/unfollow/:id", authUser, unfollow);
+router.put("/acceptRequest/:id", authUser, acceptRequest);
 
 module.exports = router;
